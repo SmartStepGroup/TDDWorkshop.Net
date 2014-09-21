87ccbb67d41748532c4f7d84d86a31f52fb987ab
ssg-dev <ssg-dev1>
2014-09-21 13:08:06 +0700

    Упражнение 1. Тесты 1-6

--- Tests/Domain.cs
+++ Tests/Domain.cs
@@ -1,4 +1,6 @@
-﻿namespace Tests
+﻿using System;
+
+namespace Tests
 {
     public class Player
     {
@@ -6,7 +8,10 @@
 
         public void Enter(Game game)
         {
+            if (currentGame != null) throw new InvalidOperationException("Можешь играть в одну игру ты только, падован");
+            game.CheckMaxPlayers();
             currentGame = game;
+            game.playerCount++;
         }
 
         public bool IsIn(Game game)
@@ -16,11 +21,20 @@
 
         public void Exit()
         {
+            if (currentGame == null) throw new InvalidOperationException("Не войдя не можешь ты выйти, юный падован");
+            currentGame.playerCount--;
             currentGame = null;
         }
     }
 
     public class Game
     {
+        public int playerCount = 0;
+
+        public void CheckMaxPlayers()
+        {
+            if (playerCount == 6) throw new InvalidOperationException("В игре число игроков максимальное"); ;
+        }
     }
+
 }
--- Tests/PlayerTests.cs
+++ Tests/PlayerTests.cs
@@ -1,4 +1,5 @@
-﻿using System.Security.Policy;
+﻿using System;
+using System.Security.Policy;
 using NUnit.Framework;
 
 namespace Tests
@@ -9,8 +10,8 @@ namespace Tests
         [Test]
         public void EnterGame_Player_InGame()
         {
-            var player = new Player();
-            var game = new Game();
+            var player = CreatePlayer();
+            var game = CreateGame();
 
             player.Enter(game);
 
@@ -20,12 +21,84 @@ namespace Tests
         [Test]
         public void ExitGame_Player_NotInGame()
         {
-            var player = new Player();
-            var game = new Game();
+            var player = CreatePlayer();
+            var game = CreateGame();
 
+            player.Enter(game);
             player.Exit();
 
             Assert.IsFalse(player.IsIn(game));
+        }
+
+        [Test]
+        public void ByDefault_ExitGame_Player_NotInGame()
+        {
+            var player = CreatePlayer();
+            var game = CreateGame();
+
+            player.Enter(game);
+            player.Exit();
+
+            var e = Assert.Throws<InvalidOperationException>(player.Exit);
+            Assert.AreEqual("Не войдя не можешь ты выйти, юный падован", e.Message);
+        }
+
+        [Test]
+        public void OneGame_Player_InGame()
+        {
+            var player = CreatePlayer();
+            var game = CreateGame();
+
+            player.Enter(game);
+
+            var e = Assert.Throws<InvalidOperationException>(() => player.Enter(game));
+            Assert.AreEqual("Можешь играть в одну игру ты только, падован", e.Message);
+        }
+
+        [Test]
+        public void JoinGameWhereHasPlayer_AnotherPlayer_InGame()
+        {
+            var player = CreatePlayer();
+            var anotherPlayer = CreatePlayer();
+            var game = CreateGame();
+
+            player.Enter(game);
+            anotherPlayer.Enter(game);
+
+            Assert.IsTrue(anotherPlayer.IsIn(game));
+        }
+
+        [Test]
+        public void EnterGame_Game_Max6Players()
+        {
+            var game = CreateGame();
+            var player = CreatePlayer();
+            var player2 = CreatePlayer();
+            var player3 = CreatePlayer();
+            var player4 = CreatePlayer();
+            var player5 = CreatePlayer();
+            var player6 = CreatePlayer();
+            var player7 = CreatePlayer();
+
+            player.Enter(game);
+            player2.Enter(game);
+            player3.Enter(game);
+            player4.Enter(game);
+            player5.Enter(game);
+            player6.Enter(game);
+
+            var e = Assert.Throws<InvalidOperationException>(() => player7.Enter(game));
+            Assert.AreEqual("В игре число игроков максимальное", e.Message);
+        }
+
+        public Game CreateGame()
+        {
+            return new Game();
+        }
+
+        public Player CreatePlayer()
+        {
+            return new Player();
         }
     }
 }
