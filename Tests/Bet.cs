6ac0572598318b924113777c9fc34bd80b106398
ssg-dev <ssg-dev1>
2014-09-21 16:56:33 +0700

    упр2-2

---
+++ Tests/Bet.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tests
+{
+    public class Bet
+    {
+        private Player owner;
+
+        public Bet(Player player)
+        {
+            owner = player;
+        }
+
+        public Player GetOwner()
+        {
+            return owner;
+        }
+    }
+}
---
+++ Tests/Game.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tests
+{
+    public class Game
+    {
+        private int playerCount = 0;
+        private List<Bet> bets = new List<Bet>();
+
+        public void Join()
+        {
+            playerCount++;
+        }
+
+        public void Leave()
+        {
+            playerCount--;
+        }
+
+        public void CheckMaxPlayers()
+        {
+            if (playerCount == 6) throw new InvalidOperationException("В игре число игроков максимальное, юный падован"); ;
+        }
+
+        public void DoBet(Bet bet)
+        {
+            if (!bet.GetOwner().IsIn(this)) throw new InvalidOperationException("Нельзя делать ставки не находясь в игре");
+            bets.Add(bet);
+        }
+
+        public List<Bet> GetBets()
+        {
+            return bets;
+        }
+    }
+}
