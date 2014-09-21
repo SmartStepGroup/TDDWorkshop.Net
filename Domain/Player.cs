b220981982412e55c6ae8c97ab21eff7e4fe98b5
unknown <Student@BOOK411L9V.win.ftc.ru>
2014-09-21 12:55:21 +0700

    CanEnter CanExit

---
+++ Domain/Player.cs
@@ -0,0 +1,32 @@
+﻿namespace Domain
+{
+    public class Player
+    {
+        private Game activeGame;
+ 
+        public bool inGame(Game game)
+        {
+            return true;
+        }
+
+        public void setActiveGame(Game game)
+        {
+            this.activeGame = game;
+        }
+
+        public Game getActiveGame()
+        {
+            return activeGame;
+        }
+
+        public void enter(Game game)
+        {
+            
+        }
+
+        public void exit()
+        {
+            setActiveGame(null);
+        }
+    }
+}
---
+++ Tests/PlayerTest.cs
@@ -0,0 +1,40 @@
+﻿using Domain;
+using NUnit.Framework;
+
+namespace Tests
+{
+    [TestFixture]
+    public class PlayerCan
+    {
+        [Test]
+        public void EnterToGame()
+        {
+            Player player = CreatePlayer();
+            Game game = CreateGame();
+
+            player.setActiveGame(game);
+
+            Assert.IsTrue(player.getActiveGame() == game);
+        }
+
+        [Test]
+        public void ExitGame()
+        {
+            Player player = CreatePlayer();
+
+            player.exit();
+
+            Assert.IsNull(player.getActiveGame());
+        }
+
+        private static Game CreateGame()
+        {
+            return new Game();
+        }
+
+        private static Player CreatePlayer()
+        {
+            return new Player();
+        }
+    }
+}
