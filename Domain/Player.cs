d92e00688245a0c15e46fc4f887aa0772f621a27
Антон Бевзюк <Kursant@BOOK411L6P.win.ftc.ru>
2014-09-21 12:18:25 +0700

    Player can enter/exit game

---
+++ Domain/Player.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Domain
+{
+    public class Player
+    {
+        private Game activeGame;
+
+        public void Enter(Game game)
+        {
+            this.activeGame = game;
+        }
+
+        public bool IsIn(Game game)
+        {
+            return game == activeGame;
+        }
+
+        public void Exit()
+        {
+            if (activeGame == null) throw new InvalidOperationException("Нельзя выйти из игры, не войдя");
+            activeGame = null;
+        }
+    }
+}
---
+++ Tests/PlayerTests.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using Domain;
+using NUnit.Framework;
+
+namespace Tests
+{
+    [TestFixture]
+    public class PlayerTests
+    {
+        [Test]
+        public void EntersGame_SinglePlayer_InGame()
+        {
+            var player = CreatePlayer();
+            var game = CreateGame();
+
+            player.Enter(game);
+
+            Assert.IsTrue(player.IsIn(game));
+        }
+
+        [Test]
+        public void ByDefault_Player_NotInGame()
+        {
+            var player = new Player();
+            var game = new Game();
+
+            Assert.IsFalse(player.IsIn(game));
+        }
+
+        [Test]
+        public void Exit_PlayerInGame_NotInGame()
+        {
+            var player = new Player();
+            var game = new Game();
+            player.Enter(game);
+
+            player.Exit();
+
+            Assert.IsFalse(player.IsIn(game));
+        }
+
+        [Test]
+        public void Exit_PlayerNotInGame_ThrowsInvalidOperationException()
+        {
+            var player = new Player();
+            
+            var e = Assert.Throws<InvalidOperationException>(player.Exit);
+            Assert.AreEqual("Нельзя выйти из игры, не войдя", e.Message);
+        }
+
+        private static Player CreatePlayer()
+        {
+            return new Player();
+        }
+
+        private static Game CreateGame()
+        {
+            return new Game();
+        }
+    }
+}
