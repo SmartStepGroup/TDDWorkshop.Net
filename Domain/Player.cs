00a3612a707d93a8d1c72145e81410a24b051e78
unknown <Student@BOOK411L9F.win.ftc.ru>
2014-09-21 12:32:43 +0700

    multutests from us

---
+++ Domain/Player.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Domain
+{
+    public class Player
+    {
+        private Game activeGame;
+        
+        public void Enter(Game game)
+        {
+            if (activeGame == game)
+                throw new InvalidOperationException("Ты уже в этой игре!");
+            if (activeGame != null)
+                throw new InvalidOperationException("Нельзя войти во вторую игру");
+           activeGame = game;
+        }
+
+        public bool IsIn(Game game)
+        {
+            return game == activeGame;
+        }
+
+        public void Exit()
+        {
+            if(activeGame==null) throw new InvalidOperationException("Нельзя просто так выйти из игры не войдя");
+            activeGame = null;
+        }
+        
+    }
+}
---
+++ Tests/PlayerTest.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using Domain;
+using NUnit.Framework;
+
+namespace Tests
+{
+    [TestFixture]
+    public class PlayerTest
+    {
+        private Player player;
+        private Game game;
+        [SetUp]
+        public void setup()
+        {
+            player = new Player();
+            game = new Game();
+        }
+        [Test]
+        public void EntersGame_SinglePlayer_InGame()
+        {
+            player.Enter(game);
+
+            Assert.IsTrue(player.IsIn(game));
+        }
+
+        [Test]
+        public void ByDefault_PlayerNotInGame()
+        {
+            Assert.IsFalse(player.IsIn(game));
+        }
+       
+        [Test]
+        public void Exit_PlayerNotInGame_ThrowInvalidOperationException()
+        {
+            var e = Assert.Throws<InvalidOperationException>(player.Exit);
+            Assert.AreEqual("Нельзя просто так выйти из игры не войдя", e.Message);
+        }
+
+        [Test]
+        public void Exit_PlayerInGame_ExitFromGame()
+        {
+            player.Enter(game);
+
+            player.Exit();
+
+            Assert.IsFalse(player.IsIn(game));
+        }
+
+        [Test]
+        public void EntersGame_EnterToSecondGame_ThrowInvalidOperationException()
+        {
+            player.Enter(game);
+            var secondGame = new Game();
+
+            var e= Assert.Throws<InvalidOperationException>(() => player.Enter(secondGame));
+
+            Assert.AreEqual("Нельзя войти во вторую игру", e.Message);
+        }
+
+        [Test]
+        public void EntersGame_EntersTheSameGame_ThrowInvalidOperationException()
+        {
+            player.Enter(game);
+
+            var e = Assert.Throws<InvalidOperationException>(() => player.Enter(game));
+
+            Assert.AreEqual("Ты уже в этой игре!", e.Message);
+        }
+    }
+}
