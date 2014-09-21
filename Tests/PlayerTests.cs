86d02a4d373e587a68a074cb86a627a1a5c21480
unknown <Student@BOOK411L7C.win.ftc.ru>
2014-09-21 11:58:33 +0700

    first test

---
+++ Tests/PlayerTests.cs
@@ -0,0 +1,36 @@
+﻿using System.Security.Policy;
+using NUnit.Framework;
+
+namespace Tests
+{
+    [TestFixture]
+    public class PlayerTests
+    {
+        [Test]
+        public void EnterGame_Player_InGame()
+        {
+            var player = new Player();
+            var game = new Game();
+
+            player.Enter();
+
+            Assert.IsTrue(player.IsIn(game));
+        }
+    }
+
+    public class Game
+    {
+    }
+
+    public class Player
+    {
+        public void Enter()
+        {
+        }
+
+        public bool IsIn(Game game)
+        {
+            return true;
+        }
+    }
+}
