07563eee0b812ce6f3d5515822332e6a87547713
ssg-dev <ssg-dev1>
2014-09-21 12:25:16 +0700

    Упражнение 1. Тесты 1-2

---
+++ Tests/Domain.cs
@@ -0,0 +1,26 @@
+﻿namespace Tests
+{
+    public class Player
+    {
+        private Game currentGame;
+
+        public void Enter(Game game)
+        {
+            currentGame = game;
+        }
+
+        public bool IsIn(Game game)
+        {
+            return currentGame == game;
+        }
+
+        public void Exit()
+        {
+            currentGame = null;
+        }
+    }
+
+    public class Game
+    {
+    }
+}
