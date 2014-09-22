a1beebe2c99572dca001c1915503457fcadd0d88
ssg-dev <ssg-dev1>
2014-09-22 09:58:27 +0700

    fix Bet.cs

---
+++ Domain/Bet.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tests
+{
+    public class Bet
+    {
+        private Player owner;
+        public int DiceValue;
+        public int ChipsCount;
+        public Guid Id;
+
+        public Bet(Player player, int diceValue, int chipsCount)
+        {
+            Id = new Guid();
+            owner = player;
+            DiceValue = diceValue;
+            ChipsCount = chipsCount;
+        }
+
+        public Player GetOwner()
+        {
+            return owner;
+        }
+    }
+}
