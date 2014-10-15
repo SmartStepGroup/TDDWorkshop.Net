2e9ced5bb5287f2f635009779b8d3254f3ec2632
Anton Bevzuk <anton.bevzuk@smartstepgroup.com>
2014-10-15 15:07:26 +0400

    Introduce duplication

--- Domain/Player.cs
+++ Domain/Player.cs
@@ -42,5 +42,14 @@ namespace Domain
         public void ReplaceBet(Bet anotherBet) {
             Bet(anotherBet);
         }
+
+        public void OneMoreBet(Bet bet) {
+            if (ActiveGame == null) throw new InvalidOperationException("Please join the game before making a bet");
+            if (bet.Score < 1 || bet.Score > 6) throw new InvalidOperationException("Please bet to a number from 1 to 6");
+            if (bet.Chips > AvailableChips) throw new InvalidOperationException("You are out of money, looser");
+
+            CurrentBet = bet;
+        }
+
     }
 }
